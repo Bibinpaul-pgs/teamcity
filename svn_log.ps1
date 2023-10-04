bcd385cfabd3dad38911aaacc9e82d690a507a46
Bibinpaul-pgs <34852070+Bibinpaul-pgs@users.noreply.github.com>
2023-10-04 22:11:56 +0530

    Add files via upload

--- svn_log.ps1
+++ svn_log.ps1
@@ -1,9 +1,12 @@
 # Define the SVN repository URL
-$svnRepoURL = "https://LAPTOP-MQIQ0NKC//svn/Project_test/"
+$svnRepoURL = "https://subversion.assembla.com/svn/codelynks^Clertrust.trustme/"
 
+# Define the SVN username and password
+$svnUsername = "bibin.paul"
+$svnPassword = "Bibin@123"
 
 # Build the full svn log command
-$svnLogCommand = "svn log $svnRepoURL"
+$svnLogCommand = "svn log $svnRepoURL --username $svnUsername --password $svnPassword"
 
 # Run the svn log command
 Invoke-Expression $svnLogCommand
